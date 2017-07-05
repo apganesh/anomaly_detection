5a956a7e443c6cef3426a7ab4f69fdea9f833106
ganesh avalur <ganeshavalur@gmail.com>
2017-07-05 05:13:41 -0700

    run.sh modification

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd src; go build -o blah; cd ..
-src/blah ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
+cd src; go build -o anomaly_detector; cd ..
+src/anomaly_detector ./log_input/batch_log.json ./log_input/stream_log.json ./log_output/flagged_purchases.json
